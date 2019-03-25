1a22bc5bfdb58f3148241e1ea72d3f7a45766944
nutrientroli <garcia_raul_alcazar@hotmail.com>
2019-03-25 23:53:49 +0100

    .

--- src/config/blackboard.js
+++ src/config/blackboard.js
@@ -1,8 +1,8 @@
 blackboard = {};
 
 blackboard.readfile = '../test.xlsx'; //readXLSX -> Input
-blackboard.list = []; //readXLSX -> Output //writeXLSX -> Input
+blackboard.list = []; //readXLSX -> Output //writeXLSX -> Input //checkIDCard -> Input
 blackboard.writefile = '../result.xlsx'; //writeXLSX -> Input
-blackboard.indexIDCard = 0;
+blackboard.indexIDCard = 0; //checkIDCard -> Input
 
 module.exports = blackboard;
